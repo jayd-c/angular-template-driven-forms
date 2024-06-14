8e4f7c464555f38b50c842b38f938e2afb3f056a
JayanDesilva <jaycdesilva@gmail.com>
2024-06-14 01:16:00 -0400

    NgForm added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-template-driven-forms';
+
+  @ViewChild('registrationForm') form: NgForm;
+
+  onFormSubmitted(){
+    console.log(this.form);
+  }
 }
