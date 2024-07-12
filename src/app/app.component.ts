bdedb5b2a61d9c75d1f05477b8e00499b42a8566
JayanDesilva <jaycdesilva@gmail.com>
2024-07-11 22:35:55 -0400

    form reset method applied when form is reset the values should be empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
   dob = '';
   emailAddress = '';
 
-
+  defaultGender = "male";
   defaultCountry = "America"
 
   @ViewChild('registrationForm') form: NgForm;
@@ -35,9 +35,17 @@ export class AppComponent {
     console.log('gender is ' + this.form.value.gender)
     console.log('first name is ' + this.form.value.firstName)
     console.log('last name is '+ this.form.controls['lastName'].value)
-    console.log('Street name ', this.form.value.address.controls.street1)
-    console.log('City name ', this.form.value.address.controls.city)
-    
+    console.log('Street name ', this.form.value.address.street1)
+    console.log('City name ', this.form.value.address.city)
+    //reset the form
+    this.form.reset();
+
+    this.form.form.patchValue({
+      gender:"male",
+      address: {
+        country:'India'
+      }
+    })
   }
 
   generateUsername() {
@@ -62,33 +70,33 @@ export class AppComponent {
     // this.form.value.username = username;
     //  this.form.controls['username'].value = username;
 
-    // this.form.setValue({
-    //   address: {
-    //     city: this.form.value.address.city,
-    //     country: this.form.value.address.country,
-    //     post: this.form.value.address.post,
-    //     region: this.form.value.address.region,
-    //     street1: this.form.value.address.street1,
-    //     street2: this.form.value.address.street2,
-    //   },
-    //   dob: this.form.value.dob,
-    //   email: this.form.value.email,
-    //   firstName: this.form.value.firstName,
-    //   gender: this.form.value.gender,
-    //   lastName: this.form.value.lastName,
-    //   phone: this.form.value.phone,
-    //   username: username
-    //  })
-
-    this.form.form.patchValue({
-      username:username,
-
-      //incase you want to assign country with value 'America'
-
+    this.form.setValue({
       address: {
-        country:'America'
-      }
-    })
+        city: this.form.value.address.city,
+        country: this.form.value.address.country,
+        postal: this.form.value.address.postal,
+        region: this.form.value.address.region,
+        street1: this.form.value.address.street1,
+        street2: this.form.value.address.street2,
+      },
+      dob: this.form.value.dob,
+      email: this.form.value.email,
+      firstName: this.form.value.firstName,
+      gender: this.form.value.gender,
+      lastName: this.form.value.lastName,
+      phone: this.form.value.phone,
+      username: username
+     })
+
+    // this.form.form.patchValue({
+    //   username:username,
+
+    //   //incase you want to assign country with value 'America'
+
+    //   // address: {
+    //   //   country:'America'
+    //   // }
+    // })
      
   }
 }
